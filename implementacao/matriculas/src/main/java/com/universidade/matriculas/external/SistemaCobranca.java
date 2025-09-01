cf8813e0c91a8c52585d98657d0b213f81c17f6c
JoaoFerreira479 <164263649+JoaoFerreira479@users.noreply.github.com>
2025-09-01 12:19:18 -0300

    Delete implementacao/matriculas directory

--- implementacao/matriculas/src/main/java/com/universidade/matriculas/external/SistemaCobranca.java
+++
@@ -1,7 +0,0 @@
-package com.universidade.matriculas.external;
-
-import com.universidade.matriculas.model.Aluno;
-
-public interface SistemaCobranca {
-	void notificarAluno(Aluno aluno, float valor);
-}
